59e7756fca51db1b257feb2be128d361ec951db2
praburangki <praburangki@gmail.com>
2024-07-16 09:53:48 +0800

    chore: remove eslint plugin vinicunca

--- .vitepress/components/core-input-number.vue
+++ .vitepress/components/core-input-number.vue
@@ -10,32 +10,34 @@ const model = defineModel();
 </script>
 
 <template>
-  <NumberFieldRoot
-    :id="label"
-    v-model="model"
-    class="text-sm"
-    :class="{
-      'opacity-50': disabled,
-    }"
-    :min="1"
-    :default-value="1"
-  >
-    <label :for="label">
-      {{ label }}
-    </label>
+  <ClientOnly>
+    <NumberFieldRoot
+      :id="label"
+      v-model="model"
+      class="text-sm"
+      :class="{
+        'opacity-50': disabled,
+      }"
+      :min="1"
+      :default-value="1"
+    >
+      <label :for="label">
+        {{ label }}
+      </label>
 
-    <div class="mt-2 flex items-center border border-base rounded-md">
-      <NumberFieldDecrement class="p-2 flex disabled:opacity-20">
-        <i class="i-radix-icons:minus" />
-      </NumberFieldDecrement>
+      <div class="mt-2 flex items-center border border-base rounded-md">
+        <NumberFieldDecrement class="p-2 flex disabled:opacity-20">
+          <i class="i-radix-icons:minus" />
+        </NumberFieldDecrement>
 
-      <NumberFieldInput
-        class="text-center w-10 tabular-nums focus:outline-2 p-1"
-      />
+        <NumberFieldInput
+          class="text-center w-10 tabular-nums focus:outline-2 p-1"
+        />
 
-      <NumberFieldIncrement class="p-2 flex disabled:opacity-20">
-        <i class="i-radix-icons:plus" />
-      </NumberFieldIncrement>
-    </div>
-  </NumberFieldRoot>
+        <NumberFieldIncrement class="p-2 flex disabled:opacity-20">
+          <i class="i-radix-icons:plus" />
+        </NumberFieldIncrement>
+      </div>
+    </NumberFieldRoot>
+  </ClientOnly>
 </template>
